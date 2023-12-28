7c23dd6c8f92a84e47e93226c1afdcbb45624376
Matt Gerega <gerega@gmail.com>
2023-12-28 10:53:55 -0500

    Added new scripts for auto-updating helm repos

---
+++ scripts/Update-FromAutoUpdate.ps1
@@ -0,0 +1,87 @@
+<#
+    .SYNOPSIS
+        Update the values.yaml file in this folder with a new image tag based on images from external sources
+
+    .DESCRIPTION
+        This script loads a file named `auto-update.json` from the folder and updates the `tagPath` in the values.yaml file
+          with a new version.
+
+        Currently this script supports checking for new release versions from a Github repository and using the tag_name from
+        that release as the image tag in the values.yaml file.
+        
+        auto-update.json sample:
+        {
+            "repository": "redis-stack/redis-stack",
+            "stripVFromVersion": false,
+            "tagPath": "redis.image.tag"
+        }
+
+
+    .EXAMPLE
+        Update-FromAutoUpdate -path c:\path\to\mychart -name mychart
+
+    .PARAMETER path
+        The path where the chart.yaml and values.yaml file exist.
+
+    .PARAMETER name
+        A descriptive name for the application, used in constructing a commit comment.
+#>
+
+param (
+    $path,
+    $name
+)
+
+Push-Location $path
+
+$error.Clear();
+
+$updateFile = Get-ChildItem "auto-update.json" -ErrorAction SilentlyContinue;
+
+if ($error.Count -gt 0) {
+    $error.Clear();
+    Pop-Location
+    return @{
+        updated = $false
+        comment = ""
+    }
+}
+
+$json = Get-Content $updateFile -Raw | ConvertFrom-Json
+
+$latest = Invoke-RestMethod -Uri "https://api.github.com/repos/$($json.repository)/releases/latest"
+
+$version = $latest.tag_name
+if ($json.stripVFromVersion) {
+    $version = $version.Trim("v");
+}
+
+
+
+$valuesYaml = Get-Content .\values.yaml -Raw | ConvertFrom-Yaml
+
+# Get current tag value    
+$expression = "`$valuesYaml.$($json.tagPath)"
+$currentVersion = Invoke-Expression $expression
+
+$versionV = New-Object "System.Management.Automation.SemanticVersion" $version.replace("v", "")
+$currentV = New-Object "System.Management.Automation.SemanticVersion" $currentVersion.replace("v", "")
+if ($currentV -ge $versionV) {
+    Pop-Location
+    return @{
+        updated = $false
+        comment = ""
+    }
+}
+
+Write-Host "Updating $name : $currentVersion -> $version"
+$commitComment += "Bump $name from $currentVersion to $version"
+$expression = "`$valuesYaml.$($json.tagPath) = `"$version`""
+Invoke-Expression $expression
+
+ConvertTo-Yaml $valuesYaml | Set-Content .\values.yaml
+Pop-Location
+return @{
+    updated = $true
+    comment = $commitComment
+}
---
+++ scripts/Update-HelmCharts.ps1
@@ -0,0 +1,120 @@
+<#
+    .SYNOPSIS
+        Iterate over the source repositories (sourceRepos) in the base directory (basePath) and process updates for dependencies and automatic updates
+
+    .DESCRIPTION
+        The script will iterate over the source repositories and recursively search for chart.yaml files.  It will then search the helm repositories on  
+        the local machine to find updates.  If an update is available, the following occurs:
+            Chart.yaml is updated with the new version
+            Chart.lock is updated by calling helm dependency update
+
+        In addition, if an `auto-update.json` file is present, `Update-FromAutoUpdate.ps1` is run on that folder to update tasgs from external sources.
+
+        This script assumes that the `helm` command is available on your local machine.
+
+    .EXAMPLE
+        Update-HelmCharts -basePath c:\ops -sourceRepos @("repository1", "repository2")
+
+    .PARAMETER basePath
+        The base path to search for sourceRepos
+
+    .PARAMETER sourceRepos
+        An array of folder names within the basePath that correspond to GitOps Git repositories.
+#>
+
+
+param (
+    $basePath,
+    $sourceRepos = @()
+)
+
+
+
+Push-Location $basePath
+
+Write-Host "Updating Helm Repositories" -NoNewline
+Invoke-Expression "helm repo update" | Out-Null
+Write-Host " - Done"
+
+$repos = ConvertFrom-Json (Invoke-Expression "helm repo list -o json")
+
+
+foreach ($sourceRepo in $sourceRepos) {
+
+    Write-Host "Processing $sourceRepo"
+    
+    Push-Location $sourceRepo
+    Invoke-Expression "git pull"
+    
+    $baseLocation = Get-Location
+    $basePath = $baseLocation.Path
+
+    $charts = get-childitem chart.yaml -Recurse
+    $commitComment = @()
+    $hasChanges = $false
+    foreach ($chart in $charts) {
+
+        $chartDisplay = $chart.DirectoryName.Replace("$basePath\", "")
+        Write-Host "Processing $chartDisplay"
+        $yaml = ConvertFrom-Yaml (Get-Content -Raw $chart)
+        $updated = $false
+        for ($index = 0; $index -lt $yaml.dependencies.Count; $index++) {
+            $name = $yaml.dependencies[$index].name
+            $version = New-Object "System.Management.Automation.SemanticVersion" $yaml.dependencies[$index].version.replace("v", "")
+            $url = $yaml.dependencies[$index].repository
+            $repo = $repos | Where-Object {$_.url -eq $url} | Select-Object -First 1
+            
+            $searchResult = ConvertFrom-Json (Invoke-Expression "helm search repo $($repo.name)/$name -o json")
+            $singleResult = $searchResult | Where-Object { $_.name -eq "$($repo.name)/$name"} | Select-Object -First 1
+
+            $newVersion = $singleResult.version
+            $repoVersion = New-Object "System.Management.Automation.SemanticVersion" $newVersion.replace("v", "")
+
+            if ($version -lt $repoVersion) {
+                Write-Host "Updating $name : $version -> $repoVersion"
+                $commitComment += "Bump $name from $version to $repoVersion"
+                $yaml.dependencies[$index].version = $newVersion
+                $yaml.version = $newVersion
+                $updated = $true
+            }
+            elseif ($version.BuildLabel -ne $repoVersion.BuildLabel) {
+                Write-Host "Updating $name : $version -> $repoVersion"
+                $commitComment += "Bump $name from $version to $repoVersion"
+                $yaml.dependencies[$index].version = $newVersion
+                $yaml.version = $newVersion
+                $updated = $true
+            }
+        }
+
+        $githubUpdate = Invoke-Expression "$PSScriptRoot/Update-FromAutoUpdate.ps1 -path $($chart.DirectoryName) -name $chartDisplay"
+        if ($githubUpdate.updated) {
+            $updated = $true
+            $commitComment += $githubUpdate.comment
+        }
+
+        if ($updated) {
+            (ConvertTo-Yaml $yaml) | Out-String | Set-Content -path $chart.FullName;
+            $hasChanges = $true
+            Push-Location $chart.DirectoryName
+            Write-Host "Updating dependencies" -NoNewline
+            Invoke-Expression "helm dependency update" | Out-Null
+            Write-Host " - Done"
+            Pop-Location
+        }
+    }
+
+    if ($hasChanges) {
+        $commitComment = $commitComment | Sort-Object -Unique
+        $commitComment = $commitComment -join "`n"
+        Write-Host "Committing changes"
+        Invoke-Expression "git add ."
+        Invoke-Expression "git commit -m `"$commitComment`""
+        Invoke-Expression "git push"
+    }
+    else {
+        Write-Host "No changes detected"
+    }
+    Pop-Location
+}
+
+Pop-Location
