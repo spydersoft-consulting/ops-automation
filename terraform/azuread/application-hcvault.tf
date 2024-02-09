66a40d4c318310edba1712e9aa07e3a5bcc461f5
Matt Gerega <gerega@gmail.com>
2024-02-09 14:43:06 -0500

    Add new application for Hashicorp Vault

---
+++ terraform/azuread/application-hcvault.tf
@@ -0,0 +1,39 @@
+resource "azuread_application" "hcvault" {
+  display_name            = "Hashicorp Vault"
+  group_membership_claims = ["All"]
+  identifier_uris         = ["https://hcvault.mattgerega.net"]
+
+  optional_claims {
+    access_token {
+      essential             = false
+      name                  = "groups"
+      additional_properties = []
+    }
+    id_token {
+      essential             = false
+      name                  = "groups"
+      additional_properties = []
+    }
+    saml2_token {
+      essential             = false
+      name                  = "groups"
+      additional_properties = []
+    }
+  }
+
+  required_resource_access {
+    resource_app_id = data.azuread_service_principal.graph.client_id
+    resource_access {
+      id   = data.azuread_service_principal.graph.oauth2_permission_scope_ids["User.Read"]
+      type = "Scope"
+    }
+  }
+
+  web {
+    redirect_uris = ["https://hcvault.mattgerega.net/ui/vault/auth/oidc/oidc/callback"]
+    implicit_grant {
+      access_token_issuance_enabled = false
+      id_token_issuance_enabled     = true
+    }
+  }
+}
