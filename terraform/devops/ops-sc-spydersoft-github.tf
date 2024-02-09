dd4270a56c1060b7f06b671add598ae8808c5ed5
Matt Gerega <gerega@gmail.com>
2024-02-09 14:50:55 -0500

    Cannot change service connection

--- terraform/devops/ops-sc-spydersoft-github.tf
+++ terraform/devops/ops-sc-spydersoft-github.tf
@@ -1,4 +1,5 @@
 resource "azuredevops_serviceendpoint_github" "spydersoft-consulting" {
   project_id            = azuredevops_project.ops.id
   service_endpoint_name = "spydersoft-consulting"
+  description = ""
 }
