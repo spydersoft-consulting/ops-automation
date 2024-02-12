eb5d124141eecdb85234a25bcfd51a82bd5c6e40
Matt Gerega <gerega@gmail.com>
2024-02-12 16:09:51 -0500

    Added new subscription terraform project

---
+++ terraform/geregalab/key-vault-geregalab-policies.tf
@@ -0,0 +1,66 @@
+resource "azurerm_key_vault_access_policy" "geregalab-owner" {
+  key_vault_id = azurerm_key_vault.geregalab.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_user.owner.object_id
+
+  key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey",
+  ]
+
+  secret_permissions = [
+    "Backup",
+    "Delete",
+    "Get",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Set",
+  ]
+
+  storage_permissions = [
+    "Backup",
+    "Delete",
+    "DeleteSAS",
+    "Get",
+    "GetSAS",
+    "List",
+    "ListSAS",
+    "Purge",
+    "Recover",
+    "RegenerateKey",
+  ]
+
+  certificate_permissions = [
+    "Backup",
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "Purge",
+    "Recover",
+    "SetIssuers",
+    "Update",
+  ]
+}
---
+++ terraform/geregalab/key-vault-geregalab.tf
@@ -0,0 +1,11 @@
+resource "azurerm_key_vault" "geregalab" {
+  name                        = "geregalab"
+  location                    = azurerm_resource_group.secrets.location
+  resource_group_name         = azurerm_resource_group.secrets.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
