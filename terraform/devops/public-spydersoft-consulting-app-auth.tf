799bca23c573e9c9878fced7d1d6af3243fd279b
Matt Gerega <gerega@gmail.com>
2024-02-14 16:26:01 -0500

    Add Tech Radar pipelines to terraform

---
+++ terraform/devops/public-spydersoft-consulting-app-auth.tf
@@ -0,0 +1,4 @@
+resource "azuredevops_serviceendpoint_github" "spydersoft-consulting-app-auth" {
+  project_id            = azuredevops_project.public.id
+  service_endpoint_name = "spydersoft-consulting"
+}
