c11e3554043ec4251af7ac22d6b253af8f673327
Matt Gerega <gerega@gmail.com>
2024-02-14 16:26:01 -0500

    Add Tech Radar pipelines to terraform

---
+++ terraform/devops/public-build-techradar-data-api.tf
@@ -0,0 +1,26 @@
+resource "azuredevops_build_definition" "techradar-data-api" {
+  project_id = azuredevops_project.public.id
+  name       = "spydersoft-consulting.techradar-data-api"
+  path       = "\\Tech Radar"
+
+  ci_trigger {
+    use_yaml = true
+  }
+
+  pull_request_trigger {
+    initial_branch = "main"
+    use_yaml       = true
+    forks {
+      enabled       = true
+      share_secrets = false
+    }
+  }
+
+  repository {
+    branch_name           = "refs/heads/main"
+    service_connection_id = azuredevops_serviceendpoint_github.spydersoft-consulting-app-auth.id # Github App Connection
+    repo_type             = "GitHub"
+    repo_id               = "spydersoft-consulting/techradar-data-api"
+    yml_path              = ".devops/pipeline-build.yml"
+  }
+}
