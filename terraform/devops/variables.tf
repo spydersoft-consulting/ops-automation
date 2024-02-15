7b675edaa8cc76d5e1d127e8e916e2457313bb18
Matt Gerega <gerega@gmail.com>
2024-02-15 08:25:09 -0500

    Add variables for Git identity

--- terraform/devops/variables.tf
+++ terraform/devops/variables.tf
@@ -15,3 +15,13 @@ variable "minio_address" {
   description = "MinIO Address"
   type        = string
 }
+
+variable "git_email" {
+  description = "Email Address associated with Git commits"
+  type        = string
+}
+
+variable "git_name" {
+  description = "Name associated with Git commits"
+  type        = string
+}
