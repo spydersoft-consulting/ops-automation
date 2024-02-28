3350004a2a3c989c067a410ff368bfcdd73baf16
Matt Gerega <gerega@gmail.com>
2024-02-27 21:15:37 -0500

    Remove test group - testing pipeline

--- terraform/geregalab/resource-group-secrets.tf
+++ terraform/geregalab/resource-group-secrets.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "secrets" {
   name     = "secrets-rg"
   location = "eastus"
 }
-
-resource "azurerm_resource_group" "todelete" {
-  name     = "todelete-rg"
-  location = "eastus"
-}
