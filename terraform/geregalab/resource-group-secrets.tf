60d4c3e41cd4b755d7b0ef3eb8b592419f08b52f
Matt Gerega <gerega@gmail.com>
2024-02-27 21:12:55 -0500

    Add test resource group to delete

--- terraform/geregalab/resource-group-secrets.tf
+++ terraform/geregalab/resource-group-secrets.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "secrets" {
   name     = "secrets-rg"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "todelete" {
+  name     = "todelete-rg"
+  location = "eastus"
+}
