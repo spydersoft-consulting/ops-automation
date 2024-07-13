5c2250ddc5c45e4569b57a3b65a054346b64e76c
Matt Gerega <gerega@gmail.com>
2024-07-13 10:59:30 -0400

    new bases

--- terraform/devops/ops-build-ubuntu-base.tf
+++ terraform/devops/ops-build-ubuntu-base.tf
@@ -1,6 +1,6 @@
-resource "azuredevops_build_definition" "build-ubuntu-base" {
+resource "azuredevops_build_definition" "build-ubuntu-base-2204" {
   project_id = azuredevops_project.ops.id
-  name       = "Build Ubuntu Base Image"
+  name       = "Build Ubuntu Base Image - 22.04"
   path       = "\\"
 
   ci_trigger {
@@ -21,6 +21,33 @@ resource "azuredevops_build_definition" "build-ubuntu-base" {
     service_connection_id = azuredevops_serviceendpoint_github.spydersoft-consulting.id
     repo_type             = "GitHub"
     repo_id               = "spydersoft-consulting/ops-automation"
-    yml_path              = ".devops/pipeline-buildbase.yaml"
+    yml_path              = ".devops/pipeline-build-ubuntu2204.yaml"
+  }
+}
+
+resource "azuredevops_build_definition" "build-ubuntu-base-2404" {
+  project_id = azuredevops_project.ops.id
+  name       = "Build Ubuntu Base Image - 24.04"
+  path       = "\\"
+
+  ci_trigger {
+    use_yaml = true
+  }
+
+  pull_request_trigger {
+    initial_branch = "main"
+    use_yaml       = true
+    forks {
+      enabled       = true
+      share_secrets = false
+    }
+  }
+
+  repository {
+    branch_name           = "refs/heads/main"
+    service_connection_id = azuredevops_serviceendpoint_github.spydersoft-consulting.id
+    repo_type             = "GitHub"
+    repo_id               = "spydersoft-consulting/ops-automation"
+    yml_path              = ".devops/pipeline-build-ubuntu2404.yaml"
   }
 }
