10143bcd1db05caab883f877f2fc7dcceb9bddb1
Matt Gerega <gerega@gmail.com>
2024-11-07 11:18:09 -0500

    Add new pipeline for Core Hosting

---
+++ terraform/devops/public-build-spydersoft-core-hosting.tf
@@ -0,0 +1,26 @@
+resource "azuredevops_build_definition" "spydersoft-core-hosting" {
+  project_id = azuredevops_project.public.id
+  name       = "spydersoft-core-hosting"
+  path       = "\\Libraries"
+
+  ci_trigger {
+    use_yaml = true
+  }
+
+  pull_request_trigger {
+    initial_branch = "main"
+    use_yaml       = true
+    forks {
+      enabled       = true
+      share_secrets = false
+    }
+  }
+
+  repository {
+    branch_name           = "refs/heads/main"
+    service_connection_id = azuredevops_serviceendpoint_github.spyder007-app-auth.id # Github App Connection
+    repo_type             = "GitHub"
+    repo_id               = "spydersoft-consulting/core-hosting"
+    yml_path              = ".devops/pipeline-main.yml"
+  }
+}
