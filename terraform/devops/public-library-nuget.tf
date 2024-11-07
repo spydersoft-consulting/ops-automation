9aab5ffda4a7e862b7d445ddab58188ea64dc4a0
Matt Gerega <gerega@gmail.com>
2024-11-07 14:27:41 -0500

    Moved nuget api key to devops variable group

---
+++ terraform/devops/public-library-nuget.tf
@@ -0,0 +1,18 @@
+
+resource "azuredevops_variable_group" "nuget-spydersoft-develop" {
+  project_id   = azuredevops_project.ops.id
+  name         = "nuget-spydersoft-develop"
+  description  = "[terraform-managed] Spydersoft Develop Nuget Credentials"
+  allow_access = false
+
+  variable {
+    name  = "nuget-feed-url"
+    value = data.vault_kv_secret_v2.proget-nuget.data["feedUrl"]
+  }
+
+  variable {
+    name         = "nuget-feed-api-key"
+    secret_value = data.vault_kv_secret_v2.proget-nuget.data["pushApiKey"]
+    is_secret    = true
+  }
+}
