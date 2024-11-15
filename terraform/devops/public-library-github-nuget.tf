0ee9af91b09cf253a0fca43b1718d510c57d1103
Matt Gerega <gerega@gmail.com>
2024-11-14 21:04:26 -0500

    Updated settings for github package publishing

---
+++ terraform/devops/public-library-github-nuget.tf
@@ -0,0 +1,18 @@
+
+resource "azuredevops_variable_group" "nuget-spydersoft-develop" {
+  project_id   = azuredevops_project.public.id
+  name         = "nuget-spydersoft-github"
+  description  = "[terraform-managed] Spydersoft Develop Nuget Credentials"
+  allow_access = false
+
+  variable {
+    name  = "nuget-feed-url"
+    value = data.vault_kv_secret_v2.github-nuget.data["feedUrl"]
+  }
+
+  variable {
+    name         = "nuget-feed-api-key"
+    secret_value = data.vault_kv_secret_v2.github-nuget.data["pushApiKey"]
+    is_secret    = true
+  }
+}
--- terraform/devops/vault-infrastructure-secrets.tf
+++ terraform/devops/vault-infrastructure-secrets.tf
@@ -26,6 +26,11 @@ data "vault_kv_secret_v2" "spydersoft-gh-token" {
   name  = "github/spydersoft-ado"
 }
 
+data "vault_kv_secret_v2" "github-nuget" {
+  mount = "secrets-infra"
+  name  = "github/nuget"
+}
+
 data "vault_kv_secret_v2" "sonarqube-ado-public-projects" {
   mount = "secrets-infra"
   name  = "sonarqube/ado-public-projects"
