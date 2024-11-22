7e4f9e9fd9702b8f6f1ef500845fc4788c266aa4
Matt Gerega <gerega@gmail.com>
2024-11-22 17:14:12 -0500

    shorten repo name

--- terraform/gh-spydersoft/actions-variables.tf
+++ terraform/gh-spydersoft/actions-variables.tf
@@ -1,5 +1,5 @@
 resource "github_actions_variable" "windows_dns_api_nuget" {
-  repository    = "spyder007/windows-dns-api"
+  repository    = "windows-dns-api"
   variable_name = "NUGET_URL"
   value         = "https://nuget.pkg.github.com/spydersoft-consulting/index.json"
 }
