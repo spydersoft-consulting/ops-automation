ad0573c23ab36e35891876e2f6727afb2da8c285
Matt Gerega <gerega@gmail.com>
2024-11-22 18:56:22 -0500

    Update token secrets

--- terraform/gh-spydersoft/actions-secrets.tf
+++ terraform/gh-spydersoft/actions-secrets.tf
@@ -1,5 +1,11 @@
 resource "github_actions_secret" "windows_dns_api_user" {
-  repository      = "spyder007/windows-dns-api"
+  repository      = "windows-dns-api"
   secret_name     = "RESOURCE_USER"
   plaintext_value = data.vault_kv_secret_v2.github-terraform.data["user"]
+}
+
+resource "github_actions_secret" "windows_dns_api_user" {
+  repository      = "windows-dns-api"
+  secret_name     = "RESOURCE_TOKEN"
+  plaintext_value = data.vault_kv_secret_v2.github-terraform.data["token"]
 }
