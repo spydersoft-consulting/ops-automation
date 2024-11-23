4f57784ea7c942a7da88f8cffa675736ffc5eabc
Matt Gerega <gerega@gmail.com>
2024-11-22 19:36:47 -0500

    Add secrets for hyperv-info-api

--- terraform/gh-spydersoft/actions-secrets.tf
+++ terraform/gh-spydersoft/actions-secrets.tf
@@ -9,3 +9,15 @@ resource "github_actions_secret" "windows_dns_api_token" {
   secret_name     = "RESOURCE_TOKEN"
   plaintext_value = data.vault_kv_secret_v2.github-terraform.data["token"]
 }
+
+resource "github_actions_secret" "hyperv_info_api_user" {
+  repository      = "hyperv-info-api"
+  secret_name     = "RESOURCE_USER"
+  plaintext_value = data.vault_kv_secret_v2.github-terraform.data["user"]
+}
+
+resource "github_actions_secret" "hyperv_info_api_token" {
+  repository      = "hyperv-info-api"
+  secret_name     = "RESOURCE_TOKEN"
+  plaintext_value = data.vault_kv_secret_v2.github-terraform.data["token"]
+}
--- terraform/gh-spydersoft/actions-variables.tf
+++
@@ -1,5 +0,0 @@
-resource "github_actions_variable" "windows_dns_api_nuget" {
-  repository    = "windows-dns-api"
-  variable_name = "NUGET_URL"
-  value         = "https://nuget.pkg.github.com/spydersoft-consulting/index.json"
-}
