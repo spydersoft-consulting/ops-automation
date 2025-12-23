ed11d6eb713e57a5f8ef0cf4c8030ea94dea7bc2
Matt Gerega <geregam@gmail.com>
2025-12-23 10:49:46 -0500

    Remove Minio Secrets

--- terraform/azuread/ad-groups.tf
+++ terraform/azuread/ad-groups.tf
@@ -1,10 +1,3 @@
-resource "azuread_group" "minio-admins" {
-  display_name     = "minio-admins"
-  description      = "Administrators for MinIO"
-  security_enabled = true
-  owners           = [data.azuread_user.owner.object_id]
-  members          = [data.azuread_user.owner.object_id]
-}
 
 resource "azuread_group" "argo-admins" {
   display_name     = "argo-admins"
