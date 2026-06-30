a9a91bd432fb7575ec25c919c57339c49a9b98e4
Matt Gerega <geregam@gmail.com>
2026-06-30 12:59:21 -0400

    Remove cluster configs

--- terraform/devops/ops-library-cluster-configs.tf
+++ terraform/devops/ops-library-cluster-configs.tf
@@ -1,21 +1,21 @@
-resource "azuredevops_variable_group" "cluster-nonprod" {
-  project_id   = azuredevops_project.ops.id
-  name         = "cluster-nonprod"
-  description  = "[terraform-managed] Nonprod cluster credentials for cycling pipeline"
-  allow_access = false
+# resource "azuredevops_variable_group" "cluster-nonprod" {
+#   project_id   = azuredevops_project.ops.id
+#   name         = "cluster-nonprod"
+#   description  = "[terraform-managed] Nonprod cluster credentials for cycling pipeline"
+#   allow_access = false
 
-  variable {
-    name         = "kubeconfig"
-    secret_value = data.vault_kv_secret_v2.k8-nonprod-kubeconfig.data["kubeconfig"]
-    is_secret    = true
-  }
+#   variable {
+#     name         = "kubeconfig"
+#     secret_value = data.vault_kv_secret_v2.k8-nonprod-kubeconfig.data["kubeconfig"]
+#     is_secret    = true
+#   }
 
-  # variable {
-  #   name         = "node-token"
-  #   secret_value = data.vault_kv_secret_v2.k8-nonprod-node-token.data["token"]
-  #   is_secret    = true
-  # }
-}
+#   # variable {
+#   #   name         = "node-token"
+#   #   secret_value = data.vault_kv_secret_v2.k8-nonprod-node-token.data["token"]
+#   #   is_secret    = true
+#   # }
+# }
 
 # resource "azuredevops_variable_group" "cluster-internal" {
 #   project_id   = azuredevops_project.ops.id
@@ -36,21 +36,21 @@ resource "azuredevops_variable_group" "cluster-nonprod" {
 #   # }
 # }
 
-resource "azuredevops_variable_group" "cluster-prod" {
-  project_id   = azuredevops_project.ops.id
-  name         = "cluster-prod"
-  description  = "[terraform-managed] Production cluster credentials for cycling pipeline"
-  allow_access = false
+# resource "azuredevops_variable_group" "cluster-prod" {
+#   project_id   = azuredevops_project.ops.id
+#   name         = "cluster-prod"
+#   description  = "[terraform-managed] Production cluster credentials for cycling pipeline"
+#   allow_access = false
 
-  variable {
-    name         = "kubeconfig"
-    secret_value = data.vault_kv_secret_v2.k8-prod-kubeconfig.data["kubeconfig"]
-    is_secret    = true
-  }
+#   variable {
+#     name         = "kubeconfig"
+#     secret_value = data.vault_kv_secret_v2.k8-prod-kubeconfig.data["kubeconfig"]
+#     is_secret    = true
+#   }
 
-  # variable {
-  #   name         = "node-token"
-  #   secret_value = data.vault_kv_secret_v2.k8-prod-node-token.data["token"]
-  #   is_secret    = true
-  # }
-}
+#   # variable {
+#   #   name         = "node-token"
+#   #   secret_value = data.vault_kv_secret_v2.k8-prod-node-token.data["token"]
+#   #   is_secret    = true
+#   # }
+# }
