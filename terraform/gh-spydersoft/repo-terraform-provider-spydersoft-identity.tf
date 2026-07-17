65480bd45f91b4090bb3c95d9f4918ce17c36775
Matt Gerega <geregam@gmail.com>
2026-07-17 10:01:06 -0400

    Provision new terraform-provider-spydersoft-identity GitHub repo

---
+++ terraform/gh-spydersoft/repo-terraform-provider-spydersoft-identity.tf
@@ -0,0 +1,60 @@
+resource "github_repository" "terraform_provider_spydersoft_identity" {
+  name                        = "terraform-provider-spydersoft-identity"
+  description                 = "Terraform/OpenTofu provider for the Spydersoft Identity Server admin API"
+  allow_auto_merge            = false
+  allow_merge_commit          = true
+  allow_rebase_merge          = true
+  allow_squash_merge          = true
+  allow_update_branch         = true
+  archived                    = false
+  auto_init                   = false
+  delete_branch_on_merge      = true
+  has_discussions             = false
+  has_downloads               = true
+  has_issues                  = true
+  has_projects                = true
+  has_wiki                    = true
+  is_template                 = false
+  topics                      = []
+  visibility                  = "public"
+  vulnerability_alerts        = true
+  web_commit_signoff_required = false
+
+  security_and_analysis {
+    secret_scanning {
+      status = "disabled"
+    }
+    secret_scanning_push_protection {
+      status = "disabled"
+    }
+  }
+}
+
+resource "github_branch_protection" "terraform_provider_spydersoft_identity_main" {
+  repository_id = github_repository.terraform_provider_spydersoft_identity.node_id
+  pattern       = "main"
+
+  allows_deletions                = false
+  allows_force_pushes             = false
+  enforce_admins                  = false
+  force_push_bypassers            = []
+  lock_branch                     = false
+  require_conversation_resolution = true
+  require_signed_commits          = false
+  required_linear_history         = true
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = false
+    dismissal_restrictions          = []
+    pull_request_bypassers          = []
+    require_code_owner_reviews      = true
+    require_last_push_approval      = false
+    required_approving_review_count = 1
+    restrict_dismissals             = false
+  }
+
+  required_status_checks {
+    contexts = []
+    strict   = true
+  }
+}
